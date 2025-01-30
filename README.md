ba330d2647bd5fadc392a6ff4badea1ace028f3c
dev <dev@introprg.myguest.virtualbox.org>
2025-01-30 12:21:07 +0100

    Completat pas5

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SGE_BLOC2
+![connection DATABASE.png](bloc2_DAHAO/Fotos/connection%20DATABASE.png)
+PAS 5
+Podem veure com obrim la connexió i la tanquem amb èxit, com 
+
+
+
+
+
--- bloc2_DAHAO/postgresql_python/connect.py
+++ bloc2_DAHAO/postgresql_python/connect.py
@@ -1,2 +1,18 @@
 import psycopg2
-conn = psycopg2.connect
+def connection_db():
+    conn = psycopg2.connect(
+        database="the_bear",
+        password="admin",
+        user="admin",
+        host="localhost",
+        port="5432"
+        )
+    return conn
+
+# Call the function to test the connection
+connection = connection_db()
+print(connection)
+connection.close()
+print(connection)
+
+
